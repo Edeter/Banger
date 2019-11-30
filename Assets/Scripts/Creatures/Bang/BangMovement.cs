65becd7954a926418427e1c38750a61d4287cf9f
Edeter <jakubeklech@o2.pl>
2019-11-30 14:13:07 +0100

    Added test assets

---
+++ Assets/Scripts/Creatures/Bang/BangMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental;
+
+public class BangMovement : MonoBehaviour
+{
+
+    private void OnEnable() {
+    
+    
+    
+    }
+
+
+    // Update is called once per frame
+    void Update(){
+        
+
+
+    }
+}
