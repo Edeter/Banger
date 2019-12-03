6b288d40860fe4691a9d5762f52317388e9b0eb4
Edeter <jakubeklech@o2.pl>
2019-12-03 23:55:43 +0100

    Prototype prototyped

---
+++ Assets/Scripts/Creatures/Bang/Body.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Body : Creature
+{
+    Rigidbody rb;
+    public float MovementMagnitude =1;
+    
+ 
+
+
+    
+private void OnEnable() {
+    if(rb==null)
+            {
+            rb = gameObject.GetComponent<Rigidbody>();
+            }
+        }
+
+void MoveDir(Vector3 dir)
+{
+    rb.velocity = dir*MovementMagnitude;
+    //rb.MovePosition(transform.position+dir*MovementMagnitude);
+    //rb.AddForce(dir*MovementMagnitude,ForceMode.Force);
+}
+
+Vector3 xaxis;
+Vector3 yaxis;
+private void Update() {
+ if(Input.anyKey)
+ {
+     MoveDir(new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical")));
+    Current = States.Moving;
+ }
+ else
+ { 
+    Current= States.Standing;
+ 
+ }
+
+    switch(Current){
+        case (States.Standing): rb.velocity = new Vector3(0,0,0);
+        break;
+        case (States.Moving):
+        break;
+        
+    }
+
+
+}
+private void OnDrawGizmos() {
+    Gizmos.color = Color.green;
+    Gizmos.DrawLine(transform.position,transform.position+new Vector3(0,0,10));
+}
+
+}
---
+++ Assets/Scripts/Creatures/Bang/Sword/Sword.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Creature
+{
+    Rigidbody _rigidbody;
+
+    public Rigidbody target;
+
+    public Transform pivot;
+ 
+
+    public Body body;
+    public float SwordMagnitude = 1;
+
+
+    
+private void OnEnable() {
+    if(_rigidbody==null)
+            {
+           _rigidbody = gameObject.GetComponent<Rigidbody>();
+            }
+
+            _rigidbody.maxAngularVelocity =1;
+}
+
+void ForceTowards(Rigidbody rb,Vector3 des)
+{
+    rb.AddForceAtPosition(des*SwordMagnitude,pivot.position,ForceMode.Force);
+}
+
+//  public void rotateRigidBodyAroundPointBy(Rigidbody rb, Vector3 origin, Vector3 axis, float angle)
+//  {
+//      Quaternion q = Quaternion.AngleAxis(angle, axis);
+//      rb.MovePosition(q * (rb.transform.position - origin) + origin);
+//      rb.MoveRotation(rb.transform.rotation * q);
+//      rb.transform.LookAt(pivot.position,Vector3.forward);
+//  }
+
+Ray ray;
+RaycastHit hit;
+private void Update() {
+    Current = body.Current;
+
+    switch(Current){
+        case (States.Standing): _rigidbody.angularDrag = Mathf.Infinity;
+        break;
+        case (States.Moving): _rigidbody.angularDrag = 5;
+        _rigidbody.AddForceAtPosition(-target.velocity,pivot.position,ForceMode.Force);
+        break;
+        case(States.Swording): _rigidbody.angularDrag = 0;
+        break;
+    }
+    
+    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+  
+             if (Physics.Raycast(ray, out hit))
+             {
+                 if (Input.GetMouseButton(0))
+                 {
+                     ForceTowards(_rigidbody,hit.point);
+                     Current = States.Swording;
+                 }
+                 
+                 
+
+                 //Debug.Log(hit);
+             }
+}
+
+private void OnGUI() {
+    GUI.Label(new Rect(0,0,100,100),hit.point.ToString());
+}
+
+private void OnDrawGizmos() {
+    Gizmos.DrawWireSphere(pivot.position,3);
+    Gizmos.DrawWireSphere(hit.point,3);
+    Gizmos.DrawWireSphere(_rigidbody.transform.position,3);
+}
+}
