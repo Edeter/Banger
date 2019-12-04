41204edd9e082b0aa2777070819e7232bf430872
Edeter <jakubeklech@o2.pl>
2019-12-04 02:03:53 +0100

    Sword movement adjusted

--- Assets/Scripts/Creatures/Bang/Sword/Sword.cs
+++ Assets/Scripts/Creatures/Bang/Sword/Sword.cs
@@ -14,6 +14,12 @@ public class Sword : Creature
     public Body body;
     public float SwordMagnitude = 1;
 
+    public float DragSpeed = 1;
+public float Deadzone;
+public float AngelAmont;
+
+float currentangularvelocity;
+
 
     
 private void OnEnable() {
@@ -22,7 +28,7 @@ private void OnEnable() {
            _rigidbody = gameObject.GetComponent<Rigidbody>();
             }
 
-            _rigidbody.maxAngularVelocity =1;
+            _rigidbody.maxAngularVelocity =100;
 }
 
 void ForceTowards(Rigidbody rb,Vector3 des)
@@ -44,13 +50,21 @@ private void Update() {
     Current = body.Current;
 
     switch(Current){
-        case (States.Standing): _rigidbody.angularDrag = Mathf.Infinity;
-        break;
-        case (States.Moving): _rigidbody.angularDrag = 5;
-        _rigidbody.AddForceAtPosition(-target.velocity,pivot.position,ForceMode.Force);
-        break;
-        case(States.Swording): _rigidbody.angularDrag = 0;
-        break;
+
+        case (States.Standing):
+                 _rigidbody.angularDrag = 40;
+            break;
+
+        case (States.Moving):
+                 _rigidbody.angularDrag = 5;
+                 _rigidbody.AddForceAtPosition(-target.velocity*DragSpeed,pivot.position,ForceMode.Force);
+            break;
+
+        case (States.Swording):
+                 _rigidbody.angularDrag = 0;
+
+                  _rigidbody.maxAngularVelocity =Mathf.Infinity;
+            break;
     }
     
     ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -59,23 +73,57 @@ private void Update() {
              {
                  if (Input.GetMouseButton(0))
                  {
-                     ForceTowards(_rigidbody,hit.point);
+                     //ForceTowards(_rigidbody,hit.point);
+                     RotateTowards();
                      Current = States.Swording;
-                 }
-                 
-                 
-
-                 //Debug.Log(hit);
+                     
+                 }else{
+                     if (currentangularvelocity!=0)
+             {
+                 currentangularvelocity/=1.1f;
              }
+                 }
+             }
+             
 }
 
+float Alpha()
+{
+return Vector3.SignedAngle(hit.point - body.gameObject.transform.position, pivot.position - body.gameObject.transform.position, Vector3.up);
+}
+void RotateTowards()
+{
+    bool AlphaGreater = Alpha() > Deadzone;
+    bool AlphaLesser = Alpha() < -Deadzone;
+    
+    if (AlphaGreater)
+    {
+        currentangularvelocity-= AngelAmont;
+    }
+    else if (AlphaLesser)
+    {
+         currentangularvelocity+=AngelAmont;
+    }
+    _rigidbody.angularVelocity = new Vector3(0,currentangularvelocity,0);
+}
+
+
 private void OnGUI() {
-    GUI.Label(new Rect(0,0,100,100),hit.point.ToString());
+    GUI.Label(new Rect(0,0,100,100),Vector3.SignedAngle(hit.point - body.gameObject.transform.position, pivot.position - body.gameObject.transform.position, Vector3.up).ToString());
+    GUI.Label(new Rect(0,15,100,100),_rigidbody.angularVelocity.ToString());
+
 }
 
 private void OnDrawGizmos() {
     Gizmos.DrawWireSphere(pivot.position,3);
     Gizmos.DrawWireSphere(hit.point,3);
-    Gizmos.DrawWireSphere(_rigidbody.transform.position,3);
+    //Gizmos.DrawWireSphere(_rigidbody.transform.position,3);
+    
+    Gizmos.color = Color.magenta;
+    Gizmos.DrawLine(body.gameObject.transform.position,pivot.position);
+
+    
+    Gizmos.color = Color.blue;
+    Gizmos.DrawLine(body.gameObject.transform.position,hit.point);
 }
 }
