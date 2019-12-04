16b214bf9111f4d4b2251afa6518c34b9290ae4c
Marcin <mpiotro4@outlook.com>
2019-12-04 21:08:45 +0100

    Gumas second test

---
+++ Assets/Sword.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float strength = 200f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) rb.AddRelativeForce(new Vector2(0, strength), ForceMode2D.Impulse);
+    }
+}
